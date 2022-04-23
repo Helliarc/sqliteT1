a1421bec83f947cfe5c4ecf167100e31b7fe87f7
Dadmin <helliarc@hotmail.com>
2022-04-23 01:20:32 +0000

    sqlite dabase created/opened

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,27 @@
 #include <iostream>
+#include "sqlite3.h"
 
 int main(int argc, char* argv[]){
   if(argc > 1){
     
   }
   else{
-    
+    sqlite3* db;
+    char* zErrMsg = 0;
+    int rc;
+
+    rc = sqlite3_open("test.db", &db);
+
+    if(rc){
+	std::cout << sqlite3_errmsg(db) << std::endl;
+	return(0);
+	}
+    else{
+	std::cout << "Opened database successfully" << std::endl;
+	}
+
+    sqlite3_close(db);
+
   }
    
   return 0;
